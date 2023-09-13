46d00ba9acc3c43c2002edd8603603b220d9a339
Aditya Singh <aditya6394@outlook.com>
2023-09-13 11:08:23 +0530

    Implemented HTTP Drills 1 using Express

---
+++ app.js
@@ -0,0 +1,47 @@
+const express = require('express');
+const html = require('./data').html;
+const jsonData = require('./data').jsonData;
+const uuidv4 = require('./data').uuidv4;
+const app = express();
+
+app.get('/html', (request, response) => {
+  response.send(html);
+});
+
+app.get('/json', (request, response) => {
+  response.send(jsonData);
+});
+
+app.get('/uuid', (request, response) => {
+  response.send(uuidv4());
+});
+
+app.get('/status/:statusCode', (request, response) => {
+  const statusCode = Number(request.params.statusCode);
+  response
+    .status(statusCode)
+    .send(`<h1> Setting the status code of response to : ${statusCode}`);
+});
+
+app.get('/delay/:delayInSeconds', (request, response) => {
+  const delay = Number(request.params.delayInSeconds);
+  setTimeout(() => {
+    response.send(`<h1>Displaying after ${delay} seconds</h1>`);
+  }, delay * 1000);
+});
+
+// =========================ERROR HANDLING=====================================
+
+// app.use((error, request, response, next) => {
+//   error.statusCode = error.statusCode || 500;
+//   error.status = error.status || 'error';
+//   response.set(error.statusCode).json({
+//     status: error.statusCode,
+//     message: error.message,
+//   });
+// });
+
+app.listen(8080, (error) => {
+  if (error) console.log("Couldn't start the server");
+  console.log('Server has been started on port 8080');
+});
